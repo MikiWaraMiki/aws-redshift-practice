233890425da4fd9ab0be7aa61c9755ce310008fc
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2022-05-22 14:33:00 +0900

    add vpc and subnet

--- lib/aws-redshift-practice-stack.ts
+++ lib/aws-redshift-practice-stack.ts
@@ -1,16 +1,36 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CfnSubnet, CfnVPC } from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsRedshiftPracticeStack extends Stack {
+  private static VPC_CIDR = '10.0.0.0/16';
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const systemName = this.node.tryGetContext('systemName');
+    const envType = this.node.tryGetContext('envType');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsRedshiftPracticeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new CfnVPC(this, 'redshiftVpc', {
+      cidrBlock: AwsRedshiftPracticeStack.VPC_CIDR,
+      tags: [
+        {
+          key: 'Name',
+          value: `${systemName}-${envType}-vpc`,
+        },
+      ],
+    });
+
+    const subnetRedshift1a = new CfnSubnet(this, 'redshiftSubnet1a', {
+      cidrBlock: '10.0.0.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1a',
+      tags: [
+        {
+          key: 'Name',
+          value: `${systemName}-${envType}-redshift-subnet-1a`,
+        },
+      ],
+    });
   }
 }
--- test/aws-redshift-practice.test.ts
+++ test/aws-redshift-practice.test.ts
@@ -1,17 +1,29 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsRedshiftPractice from '../lib/aws-redshift-practice-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as AwsRedshiftPractice from '../lib/aws-redshift-practice-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-redshift-practice-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsRedshiftPractice.AwsRedshiftPracticeStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+const app = new cdk.App();
+const stack = new AwsRedshiftPractice.AwsRedshiftPracticeStack(
+  app,
+  'AwsRedshiftPracticeStack',
+);
+const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('created vpc', () => {
+  template.resourceCountIs('AWS::EC2::VPC', 1);
+  template.hasResource('AWS::EC2::VPC', {
+    Properties: {
+      CidrBlock: '10.0.0.0/16',
+    },
+  });
+});
+
+test('have redshift subnet', () => {
+  template.resourceCountIs('AWS::EC2::Subnet', 1);
+  template.hasResource('AWS::EC2::Subnet', {
+    Properties: {
+      CidrBlock: '10.0.0.0/24',
+      AvailabilityZone: 'ap-northeast-1a',
+    },
+  });
 });
